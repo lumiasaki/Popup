e6a0dd653a2df61aa8335c64af2bdc1998eaec26
lumiasaki <edward9941@gmail.com>
2021-01-25 17:23:46 +0800

    Exclude

--- Package.swift
+++ Package.swift
@@ -26,7 +26,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Popup",
-            dependencies: ["SwiftPriorityQueue"]),
+            dependencies: ["SwiftPriorityQueue"]
+            exclude: ["Example"]
+        ),
         .testTarget(
             name: "PopupTests",
             dependencies: ["Popup"]),
